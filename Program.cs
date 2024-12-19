d13795da58ce6c03f31873cc312e66d6b9a017e8
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2024-12-19 15:48:30 -0300

    Att

--- Program.cs
+++ Program.cs
@@ -5,7 +5,14 @@ namespace Moedas
     class Program
     {
         static void Main(string[]args){
-            Console.WriteLine("Oi");
+            Console.Clear();
+            decimal valor = 10536.25m;
+            Math.Round(valor);
+            Console.WriteLine(Math.Round(valor));
+            Console.WriteLine(Math.Ceiling(valor));
+            Console.WriteLine(Math.Floor(valor));
+            
+
         }
 
     }
